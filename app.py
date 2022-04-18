baa7eb5f592303b1f42bf0ab858cddeb56066891
Lennart <LennartMa.19@gmail.com>
2022-04-18 12:47:23 +0200

    Add detection of rapid test as functionality

--- app.py
+++ app.py
@@ -7,8 +7,11 @@ import numpy as np
 #Initialize the Flask app
 app = Flask(__name__)
 
-# Load the most lightweight yolov5 model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5n')
+# Load a lightweight yolov5 model that detects different classes from COCO dataset
+# model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+
+# Load model that detects rapid tests only
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5s_rapid_test.pt', force_reload=True)
 
 # capture the Video from your webcam
 camera = cv2.VideoCapture(0)
@@ -24,14 +27,14 @@ def gen_frames():
 
             results = model(frame) # predict the objects in the corresponding frame
 
-            if results.pandas().xyxy[0]['name'].str.contains('bottle').any():
+            if results.pandas().xyxy[0]['name'].str.contains('rapid_test').any():
                 counter += 1
-                print("bottle is here", counter)
+                print("rapid test is here", counter)
 
-                if counter >= 40: # bottle has to be in the video for 40 frames
-                    print("the cropped image of bottle is saved")
+                if counter >= 30: # bottle has to be in the video for 40 frames
                     # We could make many more pictures, and than do a majority vote for the classification task
                     results.crop() # saves the images under runs/
+                    print("the cropped image of rapid test is saved")
                     counter = 0
             else:
                 counter = 0
