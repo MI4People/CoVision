682a24659692877323ed8c4f54577b6db0af2b5b
Simon Farshid <Yonom@users.noreply.github.com>
2022-04-21 01:29:21 +0200

    Init covision-app

---
+++ covision-app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.covision',
+  appName: 'covision-app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
