e434dc417ba2aceaac6015caf9fecf5187b99561
raphi777 <45787945+raphi777@users.noreply.github.com>
2022-04-22 21:29:45 +0200

    updated styling

--- covision-app/src/pages/TestInstructionOverview.tsx
+++ covision-app/src/pages/TestInstructionOverview.tsx
@@ -3,8 +3,16 @@ import { IonButton } from '@ionic/react';
 const TestInstructionOverview: React.FC = () => {
   return (
     <>
-      <IonButton href="/testInstruction/0">Hotgen Test</IonButton>
-      <IonButton href="/testInstruction/1">Another Test</IonButton>
+      <div>
+        <IonButton expand="block" href="/testInstruction/0">
+          Hotgen Test
+        </IonButton>
+      </div>
+      <div>
+        <IonButton expand="block" href="/testInstruction/1">
+          Another Test
+        </IonButton>
+      </div>
     </>
   );
 };
